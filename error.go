bc5301d8a12cd95d0b262222b16303a1d2b181c1
qualia-5w4 <geniusnaru@gmail.com>
2024-02-19 00:57:32 +0900

    feat: add known error numbers

--- error.go
+++ error.go
@@ -1,1 +1,8 @@
 package gravity
+
+var (
+	ErrNoInvalidParams             = -3
+	ErrNoSuccess                   = 0
+	ErrNoEmailAddressOrPassword    = 50012
+	ErrNoEmailAddressNotRegistered = 50014
+)
