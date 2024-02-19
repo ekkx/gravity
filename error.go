8cd48b3458ae474cfc316256e5c3aa9b429e4264
qualia-5w4 <geniusnaru@gmail.com>
2024-02-19 19:52:17 +0900

    feat: add custom errors

--- error.go
+++ error.go
@@ -1,5 +1,12 @@
 package gravity
 
+import "errors"
+
+var (
+	ErrInvalidIdentifier   = errors.New("not a valid identifier given")
+	ErrStorageDoesNotMatch = errors.New("storage does not match given credentials")
+)
+
 var (
 	ErrNoInvalidParams             = -3
 	ErrNoSuccess                   = 0
