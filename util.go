a8843db18fed0c6974c3cbce1446083d5efb187b
qualia-5w4 <geniusnaru@gmail.com>
2024-02-22 03:00:48 +0900

    feat: add struct to map function

--- util.go
+++ util.go
@@ -31,3 +31,17 @@ func generateUUID() (string, error) {
 
 	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
+
+func structToMapWithJSON(data interface{}) map[string]string {
+	result := make(map[string]interface{})
+	strResult := make(map[string]string)
+
+	b, _ := Marshal(data)
+	Unmarshal(b, &result)
+
+	for key, value := range result {
+		strResult[key] = fmt.Sprintf("%v", value)
+	}
+
+	return strResult
+}
